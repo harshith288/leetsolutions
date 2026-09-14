d67bf73699300de6be17eae80db0f441042fd6e2
harshith <h23040714@gmail.com>
2026-09-14 12:49:19 +0530

    Time: 2 ms (99.87%) | Memory: 47.1 MB (34.84%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+
+        int max = 0;
+        for (int num:nums) {
+            max=Math.max(max,num);
+        }
+        int[] points=new int[max + 1];
+        for (int num:nums) {
+            points[num] += num;
+        }
+        int prev2 = 0;
+        int prev1 = 0;
+        for (int i = 1; i <= max; i++) {
+            int current = Math.max(prev1,prev2 + points[i]);
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+} 
